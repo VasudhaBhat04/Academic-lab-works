58dbb19a622df3d3316fc798e92f7d7f940b1a3f
Vasudha Bhat <vasudhabhatkp@gmail.com>
2025-09-01 09:07:56 +0530

    p11.R

--- Stat lab/p11.R
+++ Stat lab/p11.R
@@ -33,3 +33,39 @@ plot(forecast_data,
 #optional plot
 decomposed <- decompose(ts_data)
 plot(decomposed)
+
+
+#Python code to forecast monthly antidiabetic drug sales using Holt-Winters method
+ # Load libraries
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# from statsmodels.tsa.holtwinters import ExponentialSmoothing
+
+# # Load the dataset
+# df = pd.read_csv("AustraliaDrugSales.csv", parse_dates=['date'])
+# df.set_index('date', inplace=True)
+# df.index.freq = 'MS'  # MS = Month Start
+
+
+# # Build and fit the model
+# model = ExponentialSmoothing(
+#     df['value'],
+#     trend='add',
+#     seasonal='add',
+#     seasonal_periods=12
+# ).fit()
+
+# # Forecast next 24 months
+# forecast = model.forecast(24)
+
+# # Plot original, fitted, and forecast values
+# plt.figure(figsize=(12, 6))
+# plt.plot(df.index, df['value'], label='Observed', color='blue')
+# plt.plot(model.fittedvalues.index, model.fittedvalues, label='Fitted', color='red')
+# plt.plot(forecast.index, forecast, label='Forecast', color='green')
+# plt.xlabel("Time")
+# plt.ylabel("Sales ($ Millions)")
+# plt.title("Monthly Antidiabetic Drug Sales Forecast")
+# plt.legend()
+# plt.tight_layout()
+# plt.show()
